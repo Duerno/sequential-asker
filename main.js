ae10634fc74834452a8857b8616a45afcb735f8a
Felipe Almeida <felipe.almeida@tfgco.com>
2020-08-25 11:27:08 -0300

    Create app controller and update project architecture

--- main.js
+++ main.js
@@ -1,60 +1,37 @@
 // Modules to control application life and create native browser window
 const electron = require('electron')
-const fs = require('fs')
 const path = require('path')
-const YAML = require('yaml')
-const Store = require('electron-store')
 
-// Globals to manage application data
-const QA_FILE_KEY = 'qa-file'
-let appSettings = null
-let mainWindow = null
-let settingsWindow = null
+// Internal modules to control the game
+const appController = require('./src/js/controller')
 
 // Create a generic browser window, but do not load an html into it.
-function createWindow (width=800, height=600) {
+function createWindow(width = 800, height = 600) {
   // Create the browser window.
-  const mainWindow = new electron.BrowserWindow({
+  const window = new electron.BrowserWindow({
     width: width,
     height: height,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'src/js/preload.js')
     }
   })
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // window.webContents.openDevTools()
 
-  return mainWindow
+  return window
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 electron.app.whenReady().then(() => {
-  // Create the application window
-  mainWindow = createWindow()
+  // Initialize the app
+  appController.loadApp(createWindow())
 
-  // Load app settings
-  appSettings = new Store()
-
-  // Load Q&A file containg the game data
-  const { gameData, err } = loadGameData(appSettings)
-
-  // And load the html of the app
-  if (err) {
-    mainWindow.loadFile('error.html')
-  } else {
-    mainWindow.loadFile('game.html')
-  }
-
-  // Register shortcut for the preferences window
+  // Register shortcut for the game setup page
   electron.globalShortcut.register('CommandOrControl+Shift+P', () => {
-    if (!settingsWindow) {
-      settingsWindow = createWindow(400, 600)
-      settingsWindow.loadFile('preferences.html')
-    }
-    settingsWindow.focus()
+    appController.openGameSetup()
   })
 })
 
@@ -62,7 +39,7 @@ electron.app.whenReady().then(() => {
 // dock icon is clicked and there are no other windows open.
 electron.app.on('activate', function () {
   if (electron.BrowserWindow.getAllWindows().length === 0) {
-    mainWindow = createWindow()
+    appController.loadApp(createWindow())
   }
 })
 
@@ -72,30 +49,3 @@ electron.app.on('activate', function () {
 electron.app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') electron.app.quit()
 })
-
-// Load game data composed by the ame questions and answers and the game
-// end message.
-function loadGameData (appSettings) {
-  // Read game data location
-  const gameDataFilePath = appSettings.get(QA_FILE_KEY)
-  if (!gameDataFilePath) {
-    // TODO(duerno): add initial game data here
-    return { gameData: null, err: 'game data location not found' }
-  }
-
-  // Read Q&A file
-  let gameDataFile
-  try {
-    gameDataFile = fs.readFileSync(gameDataFilePath, 'utf8')
-  } catch (err) {
-    return { gameData: null, err: `file '${gameDataFilePath}' not found` }
-  }
-
-  // Parse file
-  const rawGameData = YAML.parse(gameDataFile)
-
-  // Validate file
-  // TODO(duerno): add validation here
-
-  return { gameData: rawGameData, err: null }
-}
---
+++ src/js/controller.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const fs = require('fs')
+const YAML = require('yaml')
+const Store = require('electron-store')
+
+module.exports = {
+  /*
+   * loadApp: initializes the app
+   */
+  async loadApp(window) {
+    // Set app window
+    appWindow = window
+
+    // Load app preferences
+    appPreferences = new Store()
+
+    // Load game data file
+    const { gameData, err } = loadGameData()
+
+    // Load the initial html of the app
+    if (err) {
+      appWindow.loadFile('./src/html/setup.html')
+    } else {
+      appWindow.loadFile('./src/html/game.html')
+    }
+  },
+
+  /*
+   * openGameSetup: opens the game setup page
+   */
+  async openGameSetup() {
+    // Load the game setup page
+    appWindow.loadFile('./src/html/setup.html')
+  }
+}
+
+// Module internal globals to manage application data
+let appWindow = null
+let appPreferences = null
+const GAME_DATA_KEY = 'game-data-file-location'
+
+// Load game data composed by the ame questions and answers and the game
+// end message.
+function loadGameData() {
+  // Read game data location
+  const gameDataFilePath = appPreferences.get(GAME_DATA_KEY)
+  if (!gameDataFilePath) {
+    // TODO(duerno): add initial game data here
+    return { gameData: null, err: 'invalid game data file location' }
+  }
+
+  // Read game data file
+  let gameDataFile
+  try {
+    gameDataFile = fs.readFileSync(gameDataFilePath, 'utf8')
+  } catch (err) {
+    return { gameData: null, err: `failed to find game data file '${gameDataFilePath}'` }
+  }
+
+  // Parse file
+  const rawGameData = YAML.parse(gameDataFile)
+
+  // Validate file
+  // TODO(duerno): add validation here
+
+  return { gameData: rawGameData, err: null }
+}
