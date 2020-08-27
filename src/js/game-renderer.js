a9baf7bc76b9451d37baaa86de88fe6ac8e4c4dd
Felipe Almeida <felipe.almeida@tfgco.com>
2020-08-27 00:01:21 -0300

    Improve game visual style

--- src/js/game-renderer.js
+++ src/js/game-renderer.js
@@ -4,17 +4,23 @@ ipcRenderer.send('game-page-loaded', null)
 
 ipcRenderer.on('game-insert-message', (_, arg) => {
   if (arg && arg.owner && arg.message) {
-    let newMessage = document.getElementById('conversation-table-body').insertRow().insertCell()
-    newMessage.setAttribute('class', `${arg.owner}-message`)
-    newMessage.innerText = arg.message
+    document.getElementById('messages').innerHTML += `
+      <div class="message ${arg.owner}-message">
+        <div class="message-text">
+          <div>
+            ${arg.message}
+          </div>
+        </div>
+      </div>
+    `
   }
 })
 
-document.getElementById('user-message-form').addEventListener('submit', (event) => {
+document.getElementById('input-form').addEventListener('submit', (event) => {
   event.preventDefault()
-  let userMessage = document.getElementById('user-message-input').value
+  let userMessage = document.getElementById('input-message').value
   if (userMessage && userMessage.length >= 1) {
     ipcRenderer.send('game-user-message-received', { userMessage })
   }
-  document.getElementById('user-message-input').value = ''
+  document.getElementById('input-message').value = ''
 })
